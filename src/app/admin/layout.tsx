429cc77fd011de933acb6cbeb9f805a5008c7164
Agile Content <romankusinsky@gmail.com>
2023-11-20 13:19:13 -0300

    u

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -23,21 +23,6 @@ export default function RootLayout({
         />
         <title>Content Manager</title>
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            if (window.netlifyIdentity) {
-              window.netlifyIdentity.on("init", user => {
-                if (!user) {
-                  window.netlifyIdentity.on("login", () => {
-                    document.location.href = "/admin/";
-                  });
-                }
-              });
-            }
-          `,
-          }}
-        />
       </head>
       <body>
         {children}
