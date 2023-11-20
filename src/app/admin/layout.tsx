045a20db3ea9c0a7d8c41fd17b7d5045020e95e6
Agile Content <romankusinsky@gmail.com>
2023-11-20 12:57:13 -0300

    u

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </head>
       <body>
+        {children}
         <script src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js"></script>
       </body>
     </html>
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,3 @@
 export default function Admin() {
-  return <div>test</div>;
+  return <div>tessst</div>;
 }
