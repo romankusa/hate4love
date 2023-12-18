c730932c98d3ff588e76616a8f7f7a28c3ababff
Agile Content <romankusinsky@gmail.com>
2023-12-18 10:51:39 -0300

    u

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -8,13 +8,19 @@ export default function Admin() {
         content="width=device-width, initial-scale=1.0"
       />
       <title>Content Manager</title>
-      <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      <script
+        defer
+        src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+      ></script>
       <style
         dangerouslySetInnerHTML={{
           __html: `body {background: rgb(239, 240, 244);}`,
         }}
       ></style>
-      <script src="https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js"></script>
+      <script
+        defer
+        src="https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js"
+      ></script>
     </Head>
   );
 }
