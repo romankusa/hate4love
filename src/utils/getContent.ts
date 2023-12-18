9f1f3a1ac650f3c2c839cb32bb5eb879f17fc632
Agile Content <romankusinsky@gmail.com>
2023-12-18 11:07:29 -0300

    u

--- src/utils/getContent.ts
+++ src/utils/getContent.ts
@@ -1,25 +1,25 @@
-// import HomeEn from "@/../content/en/home.json";
-// import HomeEs from "@/../content/es/home.json";
+import HomeEn from "@/../content/en/home/home.json";
+import HomeEs from "@/../content/es/home/home.json";
 
-// import LayoutEn from "@/../content/en/layout.json";
-// import LayoutEs from "@/../content/es/layout.json";
+import LayoutEn from "@/../content/en/layout/layout.json";
+import LayoutEs from "@/../content/es/layout/layout.json";
 
 import { Locale } from "./constants";
 import { isValidLocale } from "./isValidLocale";
 
 const localesMap = {
-  // [Locale.EN]: {
-  //   layout: LayoutEn,
-  //   home: HomeEn,
-  // },
-  // [Locale.ES]: {
-  //   layout: LayoutEs,
-  //   home: HomeEs,
-  // },
+  [Locale.EN]: {
+    layout: LayoutEn,
+    home: HomeEn,
+  },
+  [Locale.ES]: {
+    layout: LayoutEs,
+    home: HomeEs,
+  },
 };
 
 export const getContent = (locale: Locale): any => {
   if (!isValidLocale(locale)) return {};
 
-  // return localesMap[locale];
+  return localesMap[locale];
 };
