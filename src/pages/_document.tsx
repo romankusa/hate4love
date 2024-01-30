c4b8e32658f2cc875bcd3312a75dc5c596fb0419
Agile Content <romankusinsky@gmail.com>
2024-01-30 17:56:48 -0300

    u

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -29,6 +29,10 @@ class AppDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Open+Sans:wght@400;500;600;700;800&family=Raleway:wght@400;500;600;700&display=swap"
             rel="stylesheet"
           />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
         </Head>
         <body>
           <Main />
